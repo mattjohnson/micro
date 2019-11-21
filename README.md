0b57c9db59ed134c4b6a8bd902d8e88ebe01e5fc
Matt Johnson <mjohnson@sonatype.com>
2019-11-21 10:45:36 -0500

    License and readme

---
+++ README.md
@@ -0,0 +1,2 @@
+Simple command line program to build and push micro NXRM images
+
--- micro.py
+++ micro.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
+# Sonatype Nexus (TM) Open Source Version
+# Copyright (c) 2008-present Sonatype, Inc.
+# All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+#
+# This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+# which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+#
+# Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+# of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+# Eclipse Foundation. All other trademarks are the property of their respective owners.
+
 import click
 import os
+
 
 @click.command()
 @click.option('-b', '--build', is_flag=True, help='Flag to build project')
@@ -14,9 +26,14 @@ def cli(build, push, profiles):
         if build:
             os.system('./nxrm.groovy -f -profile "micro-{}"'.format(profile))
             os.system('docker build -t {} -f Dockerfile private/assemblies/nexus-micro/target'.format(tag))
+
+            os.system('./nxrm.groovy -f -profile "micro-{},micro-ui"'.format(profile))
+            os.system('docker build -t {}-ui -f Dockerfile private/assemblies/nexus-micro/target'.format(tag))
         if push:
             os.system('docker push {}'.format(tag))
+            os.system('docker push {}-ui'.format(tag))
     click.echo('Done!')
+
 
 if __name__ == '__main__':
     cli()
